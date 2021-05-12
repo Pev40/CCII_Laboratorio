e09d76438fe853ee5b8358cee93762277e1a7def
Piero Vizcarra Vargas <pevv2016@gmail.com>
2021-05-11 22:12:57 -0500

    Se termino el ejercicio propuesto

--- Laboratorio_03/DynamicArray.cpp
+++ Laboratorio_03/DynamicArray.cpp
@@ -12,7 +12,7 @@ DynamicArray::DynamicArray(const Point arr[], int size)
     this->size = size;
     this->arr = new Point[size];
 
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
         this->arr[i] = arr[i];
 }
 
@@ -21,23 +21,61 @@ DynamicArray::DynamicArray(const DynamicArray &o)
     this->size = o.size;
     this->arr = new Point[o.size];
 
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
         this->arr[i] = o.arr[i];
 }
 
-void DynamicArray::push_back(Point elem) {
-    Point *tmp = new Point[size+1];
-    for(int i = 0; i < size; i++)
+void DynamicArray::push_back(Point elem)
+{
+    Point *tmp = new Point[size + 1];
+    for (int i = 0; i < size; i++)
         tmp[i] = arr[i];
 
     tmp[size] = elem;
 
-    delete []arr;
+    delete[] arr;
     size += 1;
+    arr = tmp;
+}
+
+void DynamicArray::insert(Point elem, int pos)
+{
+    Point *tmp = new Point[size + 1];
+    for (int i = pos - 1; i < size; i++)
+    {
+        tmp[i + 1] = arr[i];
+    }
+
+    tmp[pos - 1] = elem;
+
+    delete[] arr;
+    size += 1;
+    arr = tmp;
+}
+
+void DynamicArray::remove(int pos)
+{
+    Point *tmp = new Point[size - 1];
+    for (int i = 0; i < size; i++)
+    {
+        if (i < pos - 1)
+        {
+            tmp[i] = arr[i];
+        }
+        else
+        {
+            if (i > pos - 1)
+            {
+                tmp[i - 1] = arr[i];
+            }
+        }
+    }
+    delete[] arr;
+    size -= 1;
     arr = tmp;
 }
 
 DynamicArray::~DynamicArray()
 {
-    delete []arr;
+    delete[] arr;
 }
